1efe40c65f30feea03dd811762f702f488bc19f6
LCHHeluo <jason19991231@gmail.com>
2023-12-09 15:42:33 +0800

    Add new feature
    Add new feature of enemy spawn rule and wave class, that set those spawn time and every wave information like enemy number.
    Add new tag of Enemy.
    Add definition of game over, and active the animation.
    Add player health, and green monster show how much health have.

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -18,7 +18,7 @@ public class GameManagerBehavior : MonoBehaviour {
             goldLabel.GetComponent<Text>().text = "GOLD: " + gold;
         }
     }
-    public Text waveLabel;//宣告波次標籤
+    public Text waveLabel;//宣告波次標籤文字
     public GameObject[] nextWaveLabels;//宣告下一波波次標籤
     public bool gameOver = false;//宣告遊戲結束旗標並設定為否
     private int wave;//宣告波次數
@@ -33,7 +33,7 @@ public class GameManagerBehavior : MonoBehaviour {
             wave = value;//讀取外部的值
             if (!gameOver)//如果遊戲沒有結束
             {
-                for (int i = 0; i < nextWaveLabels.Length; i++)//重複做波次總數的次數
+                for (int i = 0; i < nextWaveLabels.Length; i++)//重複做兩次波次變換動畫
                 {
                     nextWaveLabels[i].GetComponent<Animator>().SetTrigger("nextWave");
                     //下一波波次標籤的動畫設定觸發器為"nextWave"
@@ -42,10 +42,54 @@ public class GameManagerBehavior : MonoBehaviour {
             waveLabel.text = "WAVE: " + (wave + 1);//波次標籤的文字設定為"WAVE: " + (波次 + 1)
         }
     }
+    public Text healthLabel;//宣告玩家生命值標籤文字
+    public GameObject[] healthIndicator;//宣告玩家生命指標物件陣列(綠色小怪獸)
+    private int health;//宣告私有玩家生命值
+    public int Health//宣告公有玩家生命值
+    {
+        get
+        {
+            return health;//回傳值給外部
+        }
+        set
+        {
+            // 1
+            if (value < health)//如果外部讀取值小於玩家生命值
+            {
+                Camera.main.GetComponent<CameraShake>().Shake();
+                //觸發相機的主物件的相機震動腳本的搖動
+            }
+            // 2
+            health = value;//玩家生命值設定為外部讀取值
+            healthLabel.text = "HEALTH: " + health;//玩家生命值標籤的文字設定為"HEALTH: "+玩家生命值
+            // 3
+            if (health <= 0 && !gameOver)//如果玩家生命值不大於0且遊戲結束旗標為否
+            {
+                gameOver = true;//遊戲結束旗標設定為是
+                GameObject gameOverText = GameObject.FindGameObjectWithTag("GameOver");
+                //宣告遊戲結束文字物件並設定為全域中帶有"Game結束"標籤的物件
+                gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+                //遊戲結束文字的動畫控制器元件的"gameOver"動畫設定為是(此動畫為遊戲結束動畫不是單指輸掉的動畫)
+            }
+            // 4 
+            for (int i = 0; i < healthIndicator.Length; i++)//迴圈判斷綠色怪物數量
+            {
+                if (i < Health)//如果i小於玩家生命值
+                {
+                    healthIndicator[i].SetActive(true);//玩家生命指標陣列的第i項設定為啟動
+                }
+                else
+                {
+                    healthIndicator[i].SetActive(false);//玩家生命指標陣列的第i項設定為不啟動
+                }
+            }
+        }
+    }
     // Use this for initialization
     void Start () {
         Gold = 1000;//金幣起始值設定為1000
         Wave = 0;//波次起始值設定為0
+        Health = 5;//玩家生命值起始值設定為5
     }
 	
 	// Update is called once per frame
--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -2,17 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Wave//宣告類別波次
+{
+    public GameObject enemyPrefab;//宣告敵人預製件物件
+    public float spawnInterval = 2;//宣告生成時間間隔並設定為2秒
+    public int maxEnemies = 20;//宣告最大敵人數並設定為20
+}
 public class SpawnEnemy : MonoBehaviour {
-    public GameObject[] waypoints;//宣告路徑點陣列
-    public GameObject testEnemyPrefab;//宣告測試用的敵人預製件
+    public GameObject[] waypoints;//宣告路徑點物件陣列
+    public GameObject testEnemyPrefab;//宣告測試用的敵人預製件物件
+    public Wave[] waves;//宣告波次類別的陣列
+    public int timeBetweenWaves = 5;//宣告波次與波次間的時間並設定為5
+
+    private GameManagerBehavior gameManager;//宣告遊戲管理器
+
+    private float lastSpawnTime;//宣告最後生成時間
+    private int enemiesSpawned = 0;//宣告已經生成的敵人數量並設定為0
     // Use this for initialization
     void Start () {
-        Instantiate(testEnemyPrefab).GetComponent<MoveEnemy>().waypoints = waypoints;
+        //Instantiate(testEnemyPrefab).GetComponent<MoveEnemy>().waypoints = waypoints;
         //實例化測試用敵人並將其MoveEnemy腳本中的waypoints設定為這個物件的路徑點陣列
+
+        lastSpawnTime = Time.time;//最後生成時間設定為當前時間點
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+        //將gameManager設定為GameManager這個物件的GameManagerBehavior這個元件(腳本物件)
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        // 1
+        int currentWave = gameManager.Wave;//宣告當前波次數並設定為遊戲管理器中儲存的波次數(起始為0)
+        if (currentWave < waves.Length)//如果當前波次數小於波次物件陣列的長度
+        {
+            // 2
+            float timeInterval = Time.time - lastSpawnTime;
+            //宣告生成完後的時間間隔並設定為當前時間點減去最後生成時間點
+            float spawnInterval = waves[currentWave].spawnInterval;
+            //生成時間間隔並設定為波次物件陣列的當前波次的生成時間間隔
+            if (((enemiesSpawned == 0 && timeInterval > timeBetweenWaves) || timeInterval > spawnInterval) && enemiesSpawned < waves[currentWave].maxEnemies)
+            {//如果((敵人已經生成的數量等於0且生成完後的時間間隔大於波次間的間隔)或生成完後的時間間隔大於生成時間間隔)且敵人已經生成數小於當前波次最大敵人數)
+             //如果尚未生成且距離上次生成已經到了下一波生成的時間或是已經開始生成而到了生成下一隻的時間這兩種中其中一種狀況下尚未達到最大敵人數
+                // 3  
+                lastSpawnTime = Time.time;//最後生成時間點刷新成目前時間點(生成新的一隻敵人後記錄時間點)
+                GameObject newEnemy = (GameObject)Instantiate(waves[currentWave].enemyPrefab);
+                //宣告新的敵人物件並實例化，藍圖是當前波次中的敵人預製件
+                newEnemy.GetComponent<MoveEnemy>().waypoints = waypoints;
+                //新的敵人物件中的MoveEnemy腳本的路徑點陣列設定為這個物件的路徑點陣列
+                enemiesSpawned++;//敵人已經生成數+1
+            }
+            // 4 
+            if (enemiesSpawned == waves[currentWave].maxEnemies && GameObject.FindGameObjectWithTag("Enemy") == null)
+            {//如果敵人已經生成數達到當前波次最大敵人數且尋找全域中所有物件中帶有"Enemy"標籤的物件都是空
+             //如果當前波次已經結束且場景中已經沒有敵人
+                gameManager.Wave++;//遊戲管理器的波次+1同時處發動畫
+                gameManager.Gold = Mathf.RoundToInt(gameManager.Gold * 1.1f);
+                //遊戲管理器的金幣數設定為原來的金幣數乘上1.1，並四捨五入到整數
+                enemiesSpawned = 0;//敵人已經生成數量設定為0
+                lastSpawnTime = Time.time;//最後生成時間點刷新成目前時間點(生成新的一隻敵人後記錄時間點)
+            }
+            // 5 
+        }
+        else//如果當前波次大於等於波次物件陣列的長度
+        {
+            gameManager.gameOver = true;//遊戲管理器的遊戲結束旗標設定為是
+            GameObject gameOverText = GameObject.FindGameObjectWithTag("GameWon");
+            //宣告遊戲結束文字物件並設定為全域中帶有"GameWon"標籤的物件
+            gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+            //遊戲結束文字的動畫控制器元件的"gameOver"動畫設定為是(此動畫為遊戲結束動畫不是輸掉的動畫)
+        }
+    }
 }
